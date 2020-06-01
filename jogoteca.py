fb35ed48b2cd367acbd47362cfc4b0e2deffa613
Daniel Teixeira <danielakioteixeira@gmail.com>
2020-06-01 19:21:19 -0300

    Capítulo 4

--- jogoteca.py
+++ jogoteca.py
@@ -1,9 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route("/inicio")
-def ola():
-    return render_template('lista.html')
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
 
-app.run(host='0.0.0.0', port=8080)
+jogo1 = Jogo('super mario', 'Ação', 'Super Nintendo')
+jogo2 = Jogo('pokemon gold', 'RPG', 'Game Boy')
+jogo3 = Jogo('monster hunter', 'RPG', 'Multiplataforma')
+jogo4 = Jogo('mortal kombat', 'Luta', 'Multiplataforma')
+lista = [jogo1, jogo2, jogo3, jogo4]
+
+@app.route("/")
+def inicio():
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+@app.route("/novo")
+def novo():
+    return render_template('novo.html', titulo='Novo')
+
+@app.route("/criar", methods=['POST'])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return redirect('/')
+
+app.run(debug=True, port=8080)
